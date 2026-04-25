730c3799c38477821c11adb7d64548d3904bd1ff
Jones Torres <124335836+jonesGladtorres@users.noreply.github.com>
2026-04-25 14:36:14 -0300

    Revert "feat(makefile): Makefile completo + correções no build Docker"

--- backend/src/exams/dto/available-slots-query.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsDateString } from 'class-validator';
-
-export class AvailableSlotsQueryDto {
-  @ApiProperty({ example: '2026-05-12' })
-  @IsDateString()
-  date!: string;
-}
